df5a009b8862296b47874208afb37260a470c930
NickygenN1 <gen.n.nicky@gmail.com>
2024-05-12 21:34:22 +0700

    re-style

---
+++ main.js
@@ -0,0 +1,28 @@
+const mtfContainer = document.getElementById("mtfapp-container");
+const mtfDetails = [
+  ["Load one or many DICOM files, and wait.", "images/mtfapp/0.png"],
+  [
+    "Automatic processing of values: max, min, contrast and modulation.",
+    "images/mtfapp/1.png",
+  ],
+  ["Visualization of graphs and estimation of line pairs.", "images/mtfapp/2.png"],
+  ["Provision of information sourced from the detector.", "images/mtfapp/3.png"],
+  ["Export to .csv format for further analysis.", "images/mtfapp/4.png"],
+  ["Don't need any preprocessing.", "images/mtfapp/5.png"],
+];
+
+// mtf details
+for (let i = 0; i < mtfDetails.length; i++) {
+  let text = mtfDetails[i][0];
+  let imageUrl = mtfDetails[i][1];
+  mtfContainer.innerHTML += `
+    <div class="detail">
+        <span
+          ><p>
+            ${text}
+          </p></span
+        >
+        <img src="${imageUrl}" alt="" />
+      </div>
+    `;
+}
